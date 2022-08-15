9d67d6cffeb30c41108ba220848b64b50754f67c
estar <lichtspielhaus@icloud.com>
2022-08-15 11:08:01 +0200

    Versucht Raumbelegung in Wochenplan anzuzeigen. Bisher nicht geklappt

--- app.js
+++ app.js
@@ -4,15 +4,16 @@ const app = Vue.createApp({
         return {
             test: "hallo",
             rooms: [
-                {roomName: "großer Saal", numberOfSeats: 50, furnishings: "Flügel" },
-                {roomName: "kleiner Saal", numberOfSeats: 25, furnishings: "Beamer" },
-                {roomName: "Gruppenraum 1", numberOfSeats: 20, furnishings: "Musikanlage" },
-                {roomName: "Gruppenraum 2", numberOfSeats: 10, furnishings: "Sofas" },
-                {roomName: "Gruppenraum 3", numberOfSeats: 15, furnishings: "Musikanlage" },
-                {roomName: "Gruppenraum 4", numberOfSeats: 5, furnishings: "Sofas" }
+                {roomName: "großer Saal", numberOfSeats: 50, furnishings: "Flügel", occupancy: frei },
+                {roomName: "kleiner Saal", numberOfSeats: 25, furnishings: "Beamer" , occupancy: frei},
+                {roomName: "Gruppenraum 1", numberOfSeats: 20, furnishings: "Musikanlage", occupancy: frei },
+                {roomName: "Gruppenraum 2", numberOfSeats: 10, furnishings: "Sofas", occupancy: frei },
+                {roomName: "Gruppenraum 3", numberOfSeats: 15, furnishings: "Musikanlage", occupancy: frei },
+                {roomName: "Gruppenraum 4", numberOfSeats: 5, furnishings: "Sofas", occupancy: frei }
 
 
-            ]
+            ],
+            belegung: frei
 
         }
     },
