f5a6662f8a78a125296af04e2da7e61ec2490fb4
JoelleWebmo <jschneemann60@gmail.com>
2022-08-15 14:47:26 +0200

    weeks

--- app.js
+++ app.js
@@ -3,40 +3,30 @@ const app = Vue.createApp({
     data (){
 
         return {
-            roomChoice:'',
-            showGrosserSaal: false,
-            rooms1: [
-                {roomName: "großer Saal", numberOfSeats: 50, furnishings: "Flügel", occupancy: "frei"},
-                {roomName: "kleiner Saal", numberOfSeats: 25, furnishings: "Beamer", occupancy: "frei"},
-                {roomName: "Gruppenraum 1", numberOfSeats: 20, furnishings: "Musikanlage", occupancy: "frei"},
-                {roomName: "Gruppenraum 2", numberOfSeats: 10, furnishings: "Sofas", occupancy: "frei"},
-                {roomName: "Gruppenraum 3", numberOfSeats: 15, furnishings: "Musikanlage", occupancy: "frei"},
-                {roomName: "Gruppenraum 4", numberOfSeats: 5, furnishings: "Sofas", occupancy: "frei"}
-
-
-            ],
-            rooms2: [
-                {roomName: "großer Saal", numberOfSeats: 50, furnishings: "Flügel", occupancy: "frei"},
-                {roomName: "kleiner Saal", numberOfSeats: 25, furnishings: "Beamer", occupancy: "frei"},
-                {roomName: "Gruppenraum 1", numberOfSeats: 20, furnishings: "Musikanlage", occupancy: "frei"},
-                {roomName: "Gruppenraum 2", numberOfSeats: 10, furnishings: "Sofas", occupancy: "frei"},
-                {roomName: "Gruppenraum 3", numberOfSeats: 15, furnishings: "Musikanlage", occupancy: "frei"},
-                {roomName: "Gruppenraum 4", numberOfSeats: 5, furnishings: "Sofas", occupancy: "frei"}
+            test: 'hallo',
+            rooms: [
+                {roomName: "großer Saal", numberOfSeats: 50, furnishings: "Flügel", occupancy: frei},
+                {roomName: "kleiner Saal", numberOfSeats: 25, furnishings: "Beamer", occupancy: frei},
+                {roomName: "Gruppenraum 1", numberOfSeats: 20, furnishings: "Musikanlage", occupancy: frei},
+                {roomName: "Gruppenraum 2", numberOfSeats: 10, furnishings: "Sofas", occupancy: frei},
+                {roomName: "Gruppenraum 3", numberOfSeats: 15, furnishings: "Musikanlage", occupancy: frei},
+                {roomName: "Gruppenraum 4", numberOfSeats: 5, furnishings: "Sofas", occupancy: frei}
 
 
             ]
+
         }
     },
     methods: {
         showRoom(){
-        if (roomChoice.localeCompare("großer Saal") === 0){
-            showGrosserSaal = true }
-        }
 
 
         },
         bookRoom(){
-
+            if (this.status === "frei"){
+                this.status = "belegt"
+            }
+            else console.log('Der Raum ist bereits belegt')
 
         },
         changeBooking(){
