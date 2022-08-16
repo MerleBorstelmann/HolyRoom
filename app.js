6fecc4bc888e6183bb28217311e9dbc1d4cdec07
estar <lichtspielhaus@icloud.com>
2022-08-16 15:38:42 +0200

    app.js per drag and drop in HTML eingefügt. Erzeugt automatische Referenz (s. Website von Intellij). Hat das Problem leider nicht gelöst.

--- app.js
+++ app.js
@@ -10,9 +10,11 @@ const app = Vue.createApp({
                 {roomName: "Gruppenraum 1", numberOfSeats: 20, furnishings: "Musikanlage", occupancy: frei},
                 {roomName: "Gruppenraum 2", numberOfSeats: 10, furnishings: "Sofas", occupancy: frei},
                 {roomName: "Gruppenraum 3", numberOfSeats: 15, furnishings: "Musikanlage", occupancy: frei},
-                {roomName: "Gruppenraum 4", numberOfSeats: 5, furnishings: "Sofas", occupancy: frei}
-
-
+                {roomName: "Gruppenraum 4", numberOfSeats: 5, furnishings: "Sofas", occupancy: frei},
+                {roomName: "Gruppenraum 5", numberOfSeats: 20, furnishings: "Beamer", occupancy: frei},
+                {roomName: "Gruppenraum 6", numberOfSeats: 15, furnishings: "Musikanlage", occupancy: frei},
+                {roomName: "Chill-Area", numberOfSeats: 10, furnishings: "Sitzsäcke", occupancy: frei},
+                {roomName: "Küche", numberOfSeats: 0, furnishings: "Küche", occupancy: frei}
             ]
 
         }
